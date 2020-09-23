e9203bf9c0ede4cc7bb159322b17bada8b8e698b
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-23 21:44:36 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,15 +3,24 @@
 
 
 def count_strings(array)
+<<<<<<< HEAD
   array.count do |element|
     element.class == String 
 
 end
 
+=======
+  array.count do |string.length|
+    string.length > 0
+  end
+>>>>>>> 9775fbf6f64576aa0ec58868be06a4d65e464458
 end
 
 def count_empty_strings(array)
   array.count("")
   end
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 9775fbf6f64576aa0ec58868be06a4d65e464458
